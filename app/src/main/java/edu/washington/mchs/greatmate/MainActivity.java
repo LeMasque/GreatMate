d97ee465da33e2ca1b7bb6959079282d98a5baff
spencer walden <masquarainian@live.com>
2017-03-01 19:06:30 -0800

    oops! removed chore tracker references -- we're not doing that at this time

--- app/src/main/java/edu/washington/mchs/greatmate/MainActivity.java
+++ app/src/main/java/edu/washington/mchs/greatmate/MainActivity.java
@@ -2,6 +2,7 @@ package edu.washington.mchs.greatmate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button toManagerViewButton = (Button) findViewById(R.id.money_manager_button);
+
     }
 }
