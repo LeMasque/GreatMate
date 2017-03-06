1c5774cfda636dd344dc168938d80e7167f30b69
Christie Gan <chrisleegan@gmail.com>
2017-03-06 13:40:01 -0800

    added basic firebase to GroceryInputActivity

--- app/src/main/java/edu/washington/mchs/greatmate/MainActivity.java
+++ app/src/main/java/edu/washington/mchs/greatmate/MainActivity.java
@@ -2,16 +2,48 @@ package edu.washington.mchs.greatmate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        //get current user
+ //       final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+ //       if (user == null) {
+  //          // user auth state is changed - user is null
+  //          // launch login activity
+  //          startActivity(new Intent(MainActivity.this, LoginActivity.class));
+   //         finish();
+   //     }
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
 
         Button toManagerViewButton = (Button) findViewById(R.id.money_manager_button);
 
