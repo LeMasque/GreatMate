7a057a910f5707fc6e97b7fc0729aa2c8a34d7af
cazadori <dubeh@uw.edu>
2017-03-06 20:02:04 -0800

    merging with master after commit

--- app/src/main/java/edu/washington/mchs/greatmate/MainActivity.java
+++ app/src/main/java/edu/washington/mchs/greatmate/MainActivity.java
@@ -1,8 +1,13 @@
 package edu.washington.mchs.greatmate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null) {
+            Log.d("Main", user + "user alread logged in");
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        */
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
 
         Button toManagerViewButton = (Button) findViewById(R.id.money_manager_button);
 
